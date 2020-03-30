455a1b3e3bc46ff81d4b98ca98e8a1b3ba42f44c
Zron <ailen1002@163.com>
2020-03-30 14:14:17 +0800

    项目增加helmet安全插件

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import * as helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule,{
+    logger: ['error','warn']
+  });
+  await app.listen(3100);
+  app.enableCors();
+  app.use(helmet());
 }
 bootstrap();
