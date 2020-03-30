e32c3eb5dec1a2fb9cf27b4d103df0b998d24a0b
Zron <ailen1002@163.com>
2020-03-30 14:51:39 +0800

    加入接口说明文档swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import * as helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
     logger: ['error','warn']
   });
-  await app.listen(3100);
+  
   app.enableCors();
   app.use(helmet());
+
+  const options = new DocumentBuilder()
+    .setTitle('Test API')
+    .setDescription('a test API')
+    .setVersion('V1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-doc', app, document);
+
+  await app.listen(3100);
 }
 bootstrap();
