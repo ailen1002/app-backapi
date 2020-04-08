1cd4cb94a74360274f3643353ae62dee83e938da
Zron <ailen1002@163.com>
2020-04-08 09:28:19 +0800

    新增商品模块controller,包含商品分类controller、商品controller、订单controller

--- src/controllers/admin/admin.module.ts
+++ src/controllers/admin/admin.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WorkHistoryController } from './work-history/work-history.controller';
+import { GoodsController } from './goods/goods/goods.controller';
+import { CategoryController } from './goods/category/category.controller';
+import { OrderController } from './goods/order/order.controller';
 
 @Module({
-  controllers: [WorkHistoryController]
+  controllers: [WorkHistoryController, GoodsController, CategoryController, OrderController]
 })
 export class AdminModule {}
